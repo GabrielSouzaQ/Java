1097ad59f07dcd74d729951477bb91b7500c5e6d
GabrielSouzaQ <rm98570@gmail.com>
2023-03-21 20:54:10 -0300

    Atualização de "view"

--- 03-Metodos/src/br/com/fiap/motors/model/View.java
+++ 03-Metodos/src/br/com/fiap/motors/model/View.java
@@ -12,10 +12,24 @@ public class View {
 		gol.modelo = "Gol Bola";
 		gol.valor = 15000;
 		
+		
 		//Calcular a idade do carro
 		int i = gol.calcularIdade(2023);
 		
 		//Exibir a idade
 		System.out.println("Idade do carro: " + i);
-	}
+
+		//aumentar o valor do carro em 20%
+		gol.atualizarValor(0.2);
+
+		//exibir o valor do carro 
+		System.out.println("Valor do carro: " + gol.valor);
+
+		//Exibir o valor do seguro de acordo com o valor do carro
+		double seguro = gol.calcularSeguro();
+		System.out.println("Valor do Seguro: " + seguro);
+		
+		//Exibir o valor da parcela do seguro em 3x
+		double parcela = gol.calcularParcelas(30);
+		System.out.println("O valor da parcela do seguro (3x); " + parcela)
 }
