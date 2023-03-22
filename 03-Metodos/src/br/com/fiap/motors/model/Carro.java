683567e7282fdae5d410e2030328c90a540b3db3
GabrielSouzaQ <rm98570@gmail.com>
2023-03-21 21:28:37 -0300

    pronto

--- 03-Metodos/src/br/com/fiap/motors/model/Carro.java
+++ 03-Metodos/src/br/com/fiap/motors/model/Carro.java
@@ -18,4 +18,32 @@ public class Carro {
 		return idade;
 	}
 	
+	//aumentar o valor do carro 
+	void atualizarValor(double porcentagem){
+		valor *= (1 + porcentagem);
+
+	}
+
+	//Criar um metodo que calcula e retorna o valro do seguro do carro
+	//Seguro do carro é baseado no valor, 5% do valor do carro
+	public double calcularSeguro(double porcentagemSeguro){
+		double seguro = (valor * porcentagemSeguro);
+		return seguro;
+	}
+
+	public double calcularSeguro(){
+		double seguro = valor * 0,05;
+		return seguro;
+	}
+
+	//Criar um metodo que recebe a quantidade de parcelas, calcula e retorna
+	//o valor da parcela do seguro 
+
+	public double calcularParcelas(int numeroParcelas){
+		double seguro = calcularSeguro();
+		return seguro / numeroParcelas;
+	
+	}
+
+
 }
